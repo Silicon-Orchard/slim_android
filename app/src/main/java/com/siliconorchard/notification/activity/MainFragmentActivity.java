d07bb05339f8edf2f41233301439e3d3f020b6f0
Tamal <t.tahasin@gmail.com>
2016-07-27 20:05:52 +0600

    Duplicate notification issue resolved.

--- app/src/main/java/com/siliconorchard/notification/activity/MainFragmentActivity.java
+++ app/src/main/java/com/siliconorchard/notification/activity/MainFragmentActivity.java
@@ -123,6 +123,7 @@ public class MainFragmentActivity extends FragmentActivity{
         if(isSimilarStatusFound != null && isSimilarStatusFound) {
             showChatRequestDialog();
         }
+        isSimilarStatusFound = false;
     }
 
     @Override
